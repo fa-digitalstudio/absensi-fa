bab07f78f5040d11909ad138aebe7c1bf24200fe
Fa-digitalstudio <76058785+fa-digitalstudio@users.noreply.github.com>
2026-06-12 12:41:27 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js - Hanya berisi data konfigurasi
+const firebaseConfig = {
+  apiKey: "AIzaSyCMUw8KwCeSNY41i5EmmqKexzLp5RRMNoY",
+  authDomain: "daftar-absen-cc868.firebaseapp.com",
+  databaseURL: "https://daftar-absen-cc868-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "daftar-absen-cc868",
+  appId: "1:486404759637:web:120404fde3e0c1f1d0b4dc"
+};
+
+const ADMIN_KEY = "farid1234";
